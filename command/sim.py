2bbf4a659f056ca70e8ae5e5900f8f7abbd07364
Khangphan <102360362+KhangPhan27@users.noreply.github.com>
2023-02-14 11:53:15 +0700

    Delete command directory

--- command/sim.py
+++
@@ -1,7 +0,0 @@
-import requests, json
-from action.sendMessage import sendMessage
-from action.seen import seen
-def sim(sender_id, message):
-    seen(sender_id)
-    res = requests.get(f"https://api.simsimi.net/v2/?text={message}&lc=vn").json()
-    sendMessage(sender_id, res["success"])
